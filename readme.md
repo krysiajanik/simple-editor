564f3e617f6cb2864270f31906479febad18c54b
krysiajanik <a.krystyna.janik@gmail.com>
2020-06-21 19:26:21 +0200

    functionality added, minor change to readme

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 ![cover](./gh/cover.png)
 
 # SimpleEditor
+A simple text editor where you can store great ideas -> hence ideator!
 
 ## How to access?
 The tool can be accessed [here](https://krysiajanik.github.io/simple-editor/).
--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,53 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+let textArea = document.getElementById('textarea');
+
+
+
+//Make the style Italic
+
+const iconItalic = document.querySelector('.icons__italic--js');
+
+iconItalic.addEventListener('click', () => {
+  if (textArea.style.fontStyle == 'italic') {
+    textArea.style.fontStyle = 'normal'
+  } else
+    textArea.style.fontStyle = 'italic'
+
+});
+
+
+//Bold the text
+
+const iconBold = document.querySelector('.icons__bold--js');
+
+iconBold.addEventListener('click', () => {
+  if (textArea.style.fontWeight == 'bold') {
+    textArea.style.fontWeight = 'normal'
+  } else
+    textArea.style.fontWeight = 'bold'
+});
+
+
+
+//Save the text from textarea
+
+const buttonSave = document.querySelector('.button__save--js');
+
+buttonSave.addEventListener('click', () => {
+  localStorage.setItem('SavedText', textArea.value);
+});
+
+
+//Load the text from local storage
+
+const buttonLoad = document.querySelector('.button__load--js');
+
+buttonLoad.addEventListener('click', () => {
+  let loadText = localStorage.getItem('SavedText');
+  if (localStorage.getItem('SavedText') == null) {
+    alert('Nothing to load!')
+  } else
+  textArea.value = loadText;
+});
